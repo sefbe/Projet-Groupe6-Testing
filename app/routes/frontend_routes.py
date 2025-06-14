795e3a2a16c9e6a08eb04a29687fe74b95d04668
ronelmaamoc <135178718+ronelmaamoc@users.noreply.github.com>
2025-06-14 20:04:09 +0100

    Add files via upload

--- app/routes/frontend_routes.py
+++ app/routes/frontend_routes.py
@@ -12,17 +12,45 @@ def register():
     return render_template('register.html')
 
 @frontend.route('/vehicle')
+#@jwt_required() # Assurez-vous que l'accès au tableau de bord des véhicules nécessite une authentification
 def vehicles():
     return render_template('vehicles.html')
+
+# Nouvelle route pour la page d'ajout d'un véhicule
+@frontend.route('/add_vehicle')
+#@jwt_required() # Il est fortement recommandé de protéger cette route avec une authentification
+def add_vehicle():
+    return render_template('add_vehicle.html')
 
 
 # Détails ou édition d’un véhicule spécifique (admin)
 @frontend.route('/vehicles/<int:vehicle_id>/edit')
+#@jwt_required() # Protéger également cette route
 def edit_vehicle(vehicle_id):
     return render_template('edit_vehicle.html', vehicle_id=vehicle_id)
 
+@frontend.route('/user')
+#@jwt_required()
+def list_users():
+    return render_template('users.html')
+
+@frontend.route('/add')
+#@jwt_required()
+def add_user():
+    return render_template('add_users.html')
+
+from .user_routes import get_user_by_id
+
+@frontend.route("/edit/<int:user_id>")
+def edit_user(user_id):
+    user = get_user_by_id(user_id)
+    if not user:
+        return "Utilisateur non trouvé", 404
+    return render_template("edit_users.html", user=user)
+
+    
 # Profil de l'utilisateur connecté
 @frontend.route('/profile')
+#@jwt_required() # Protéger cette route
 def profile():
     return render_template('profile.html')
-
